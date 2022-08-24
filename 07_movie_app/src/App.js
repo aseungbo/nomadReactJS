0905a5a6b45db98c5fbeac8fdaad2c420fad2fe4
seungb5 <seungbo1112@gmail.com>
2022-08-24 12:43:47 +0900

    Add import line

--- 07_movie_app/src/App.js
+++ 07_movie_app/src/App.js
@@ -5,10 +5,12 @@ import {
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import React from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
+
+<BrowserRouter>
       <Routes>
         <Route path="/movie/:id" element={<Detail />}/>
         <Route path="/" element={<Home />}/>
