5d7b6486ffb5072ee95b970fc6f7eeeb65a49de4
seungb5 <seungbo1112@gmail.com>
2022-08-24 12:43:03 +0900

    Updates

---
+++ precache-manifest.b4b93a7f9b51c3c6c079e55b3f0e233b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5c9d6b5f1e7c2e973ad",
+    "url": "/react-for-beginners/static/js/main.73c42777.chunk.js"
+  },
+  {
+    "revision": "6873a68efa9d37e96a89",
+    "url": "/react-for-beginners/static/js/runtime~main.08ccc910.js"
+  },
+  {
+    "revision": "0097e938511e0ff3999a",
+    "url": "/react-for-beginners/static/js/2.4f29cc92.chunk.js"
+  },
+  {
+    "revision": "8a4ccd2f39d7e3e1df31873835534ccd",
+    "url": "/react-for-beginners/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-for-beginners/precache-manifest.b4b93a7f9b51c3c6c079e55b3f0e233b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-for-beginners/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
