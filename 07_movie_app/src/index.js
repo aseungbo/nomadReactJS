33181e13442a276aa070b09378b4bdfb70699277
aseungbo <seungbo1112@gmail.com>
2022-09-28 21:44:40 +0900

    Add Google Analytics

--- 07_movie_app/src/index.js
+++ 07_movie_app/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const config = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+const history = createBrowserHistory();
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+  console.log("history called");
+});
+ReactGA.initialize(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
