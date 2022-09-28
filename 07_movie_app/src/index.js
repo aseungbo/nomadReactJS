c746c16816e2d642e9f913afbbef0dda03592c9d
aseungbo <seungbo1112@gmail.com>
2022-09-28 21:58:43 +0900

    Modified initialize line

--- 07_movie_app/src/index.js
+++ 07_movie_app/src/index.js
@@ -5,13 +5,13 @@ import ReactGA from "react-ga";
 import { createBrowserHistory } from "history";
 
 const config = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+ReactGA.initialize(config);
 const history = createBrowserHistory();
 history.listen((location) => {
   ReactGA.set({ page: location.pathname });
   ReactGA.pageview(location.pathname);
   console.log("history called");
 });
-ReactGA.initialize(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
