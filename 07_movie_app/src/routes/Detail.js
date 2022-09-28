a2a3f3ed0e5b477fda390a6a4080cb0e47239a6c
aseungbo <seungbo1112@gmail.com>
2022-09-28 21:48:59 +0900

    Test msg

--- 07_movie_app/src/routes/Detail.js
+++ 07_movie_app/src/routes/Detail.js
@@ -1,21 +1,19 @@
 import { useEffect } from "react";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function Detail() {
-    const { id } = useParams();
-    const getDetail = async () => {
-        const json = await(
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-        ).json();
-        console.log(id);
-        console.log(json);
-    }
-    useEffect(() => {
-        getDetail();
-    }, [])
-    return (
-        <h1> Details </h1>
-    );  
+  const { id } = useParams();
+  const getDetail = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(id);
+    console.log(json);
+  };
+  useEffect(() => {
+    getDetail();
+  }, []);
+  return <h1> Build Complete </h1>;
 }
 
 export default Detail;
