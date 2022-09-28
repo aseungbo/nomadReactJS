d98ed330c6ae3209219d848fd3be22bab11db65c
aseungbo <seungbo1112@gmail.com>
2022-09-28 22:31:17 +0900

    Modified config

--- 07_movie_app/src/App.js
+++ 07_movie_app/src/App.js
@@ -1,18 +1,27 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import ReactGA from "react-ga";
+// import { createBrowserHistory } from "history";
+
+const config = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+ReactGA.initialize(config);
+
+// const history = createBrowserHistory();
+// history.listen((location) => {
+//   console.log("history called");
+//   ReactGA.set({ page: location.pathname });
+//   ReactGA.pageview(location.pathname);
+// });
 
 function App() {
+  console.log("App called");
+  // ReactGA.pageview(window.location.pathname + window.location.search);
   return (
-
-<BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />}/>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}/> 
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         {/* <Route path="/" element={<Home />}/> */}
       </Routes>
     </BrowserRouter>
--- 07_movie_app/src/index.js
+++ 07_movie_app/src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ReactGA from "react-ga";
-import { createBrowserHistory } from "history";
-
-const config = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
-ReactGA.initialize(config);
-const history = createBrowserHistory();
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-  console.log("history called");
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
