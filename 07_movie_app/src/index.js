bd655a70d7ae3da376cd3b6a9a9255a86216f23a
aseungbo <seungbo1112@gmail.com>
2023-06-04 11:42:51 +0900

    Add hotjar tracking code

--- 07_movie_app/src/index.js
+++ 07_movie_app/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const hotjarScript = document.createElement('script');
-hotjarScript.innerHTML = `(function(h,o,t,j,a,r){
+hotjarScript.innerHTML = ` (function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:3519293,hjsv:6};
+  h._hjSettings={hjid:3519713,hjsv:6};
   a=o.getElementsByTagName('head')[0];
   r=o.createElement('script');r.async=1;
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
